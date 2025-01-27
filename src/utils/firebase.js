1eee27ad3be5eaebc29eb7eb21bbfbfaeea1da69
anadin1001 <anadin.amd@gmail.com>
2025-01-27 11:11:55 +0200

    Remove API secrets

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,14 +6,7 @@ require("dotenv").config();
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCPexx14XI0jVr1dl7Mi0fZMPH64tKV_Gg",
-  authDomain: "single-page-crud-app.firebaseapp.com",
-  projectId: "single-page-crud-app",
-  storageBucket: "single-page-crud-app.firebasestorage.app",
-  messagingSenderId: "755446518302",
-  appId: "1:755446518302:web:9ffd831fac4eff5083d40f"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
