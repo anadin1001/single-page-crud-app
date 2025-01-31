79d8a7f88405965a14b2fec4d7e81912153ef724
anadin1001 <anadin.amd@gmail.com>
2025-01-31 17:11:54 +0200

    Firebase done

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,57 +1,80 @@
-// const Author = require('../models/Author');
-const validateData = require('../utils/validateData.js');
+const Author = require('../models/Author');
+const validateData = require("../utils/validateData.js");
+const { getFirestore } = require("firebase-admin/firestore");
+const db = getFirestore();
 
-const authors=[];
+const getAllAuthors = async (req, res) => {
+  try {
+    const authorsRef = await db.collection("authors").get();
+    const authors = [];
+    authorsRef.forEach((doc) => {
+        authors.push({
+          id: doc.id, 
+          ...doc.data(), 
+        });
+      });
 
-exports.getAllAuthors  = async(req, res)=>{
     res.status(200).json(authors);
-}
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "A apărut o eroare la citirea autorilor: " + error.message,
+      });
+  }
+};
 
-
-exports.createAuthor = (req, res) => {
-    const {name, genre, biography,email, age, nationality} = req.body;
-
-    if(!name || !genre || !biography || !age || !nationality || !email){
-        return res.status(400).json({error: "All fields are required"});
+const createAuthor = async (req, res) => {
+  try {
+    const { name, genre, biography, email, age, nationality } = req.body;
+    console.log("Received request body:", req.body);
+    if (!name || !genre || !biography || !age || !nationality || !email) {
+      return res.status(400).json({ error: "All fields are required" });
     }
- 
+
     results = validateData.validateAuthor(name);
-    if(results!==true){
-        return res.status(400).send({message: results});
+    if (results !== true) {
+      return res.status(400).send({ message: results });
     }
 
     results = validateData.validateGenre(genre);
-    if(results!==true){
-        return res.status(400).send({message: results});
+    if (results !== true) {
+      return res.status(400).send({ message: results });
     }
 
-
     results = validateData.validateBiography(biography);
-    if(results!==true){
-        return res.status(400).send({message: results});
+    if (results !== true) {
+      return res.status(400).send({ message: results });
     }
 
     results = validateData.validateEmail(email);
-    if(results!==true){
-        return res.status(400).send({message: results});
+    if (results !== true) {
+      return res.status(400).send({ message: results });
     }
 
-    try{
-        const authorJSON={
-            name : req.body.name,
-            genre: req.body.genre,
-            email:req.body.email,
-            biography: req.body.biography,
-            age: req.body.age,
-            nationality: req.body.nationality
-    
-        }
-    
-        authors.push(authorJSON);
-        res.status(201).json(authorJSON);
-    }catch (error) {
-        res.status(500).json({ message: "A apărut o eroare la crearea cărții: " + error.message });
-    }
-    
-}
+    const authorJSON = {
+      name: req.body.name,
+      genre: req.body.genre,
+      email: req.body.email,
+      biography: req.body.biography,
+      age: req.body.age,
+      nationality: req.body.nationality,
+      books:[]
+    };
 
+    const responseDB = await db.collection("authors").add(authorJSON);
+    res.status(201).json({
+      message: "Author has been created!",
+      authorId: responseDB.id,
+      authorData: authorJSON,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "A apărut o eroare la crearea cărții: " + error.message,
+      });
+  }
+};
+
+module.exports = { createAuthor, getAllAuthors };
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,83 +1,100 @@
-const db = require("../utils/firebase"); // Configurarea Firebase
-const { collection, addDoc } = require("firebase/firestore");
-const { getFirestore } = require("firebase/firestore");
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+const db = getFirestore();;
 
-const validateData = require('../utils/validateData.js');
+const validateData = require("../utils/validateData.js");
 
-let books = [];
 
-exports.getAllBooks = async(req, res)=>{
-    try {
-        res.status(200).json(books); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
-
-exports.createBook = async(req, res) => {
-
-    const {title, genre, year, description, pages } = req.body;
-
-    if(!title || !description || !genre || !year || !pages){
-        return res.status(400).json({error: "All fields are required"});
-    }
-    
-    
-    results = validateData.validateTitle(title);
-    if(results!==true){
-        return res.status(400).send({message: results});
-    }
-
-    results = validateData.validateGenre(genre);
-    if(results!==true){
-        return res.status(400).send({message: results});
-    }
-
-    results = validateData.validateYear(year);
-    if(results!==true){
-        return res.status(400).send({message: results});
-    }
-
-    results = validateData.validateDescription(description);
-    if(results!==true){
-        return res.status(400).send({message: results});
-    }
-
-    results = validateData.validatePages(pages);
-    if(results!==true){
-        return res.status(400).send({message: results});
-    }
-
+exports.getAllBooks = async (req, res) => {
     try{
-        const bookJson = {
-            title: req.body.title, 
-            genre: req.body.genre,
-            year: req.body.year,
-            description:req.body.description,
-            pages: req.body.pages
-        };
+  const authorId = req.params.id.trim();
 
-        books.push(bookJson);
-        res.status(201).json(bookJson);
+  const authorRef = await db.collection("authors").doc(authorId);
+    const author = await authorRef.get();
+
+    if (!author.exists) {
+        return res.status(404).json({ message: "Author not found" });
+        }
+
+    
+        const booksRef = await authorRef.collection("books").get();
+        const books = [];
+        booksRef.forEach((doc) => {
+            books.push({
+                id: doc.id,
+                data: doc.data(),
+            });
+        });
+
+        res.status(200).json(books);
     } catch (error) {
-        res.status(500).json({ message: "A apărut o eroare la crearea cărții: " + error.message });
+        res.status(500).json({ message: "Error getting books", error: error.message });
     }
-
-   
-
-  //To do: Add the book to the database
-    // const response = await db.collection("").add(bookJson);
-    //     res.status(201).json({
-    //         message: "Book created successfully",
-    //         medicId: response.id,
-    //         medicData: medicJson 
-    //     });
-    // } catch (error) {
-    //     res.status(500).send({
-    //         message: "Error creating book",
-    //         error: error.message
-    //     });
-    // }
-
 };
 
+exports.createBook = async (req, res) => {
+    
+    const { authorId, title, genre, year, description, pages } = req.body;
+
+    if (!authorId || !title || !description || !genre || !year || !pages) {
+        return res.status(400).json({ error: "All fields are required, including authorId" });
+    }
+
+  results = validateData.validateTitle(title);
+  if (results !== true) {
+    return res.status(400).send({ message: results });
+  }
+
+  results = validateData.validateGenre(genre);
+  if (results !== true) {
+    return res.status(400).send({ message: results });
+  }
+
+  results = validateData.validateYear(year);
+  if (results !== true) {
+    return res.status(400).send({ message: results });
+  }
+
+  results = validateData.validateDescription(description);
+  if (results !== true) {
+    return res.status(400).send({ message: results });
+  }
+
+  results = validateData.validatePages(pages);
+  if (results !== true) {
+    return res.status(400).send({ message: results });
+  }
+
+  try {
+    const authorRef = db.collection("authors").doc(authorId);
+        const authorSnapshot = await authorRef.get();
+
+        if (!authorSnapshot.exists) {
+            return res.status(404).json({ message: "Author not found" });
+        }
+
+    const bookId = db.collection("authors").doc().id;
+    const bookJson = {
+        id: bookId,
+      title: req.body.title,
+      genre: req.body.genre,
+      year: req.body.year,
+      description: req.body.description,
+      pages: req.body.pages,
+    };
+
+    await authorRef.update({
+        books: FieldValue.arrayUnion(bookJson)
+    });
+
+    res.status(201).json({
+        message: "Book added to author's book list!",
+        bookId: bookId,
+        bookData: bookJson,
+    });
+
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating book", error: error.message });
+  }
+};
