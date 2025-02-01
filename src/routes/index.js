1c9bcaaeea6f6bb59bb4d112802f79eac6fc6e29
anadin1001 <anadin.amd@gmail.com>
2025-02-01 16:49:34 +0200

    update

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Endpoint de test
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
-});
-
-module.exports = router;
